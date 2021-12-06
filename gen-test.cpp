f5485195099103aa296747c225f9b6ca9b4cf678
Ales Cepek <cepek@gnu.org>
2021-12-06 06:17:11 +0100

    first draft of output to two different streams currently output files are identical (testing version for diff check)

--- gen-test.cpp
+++ gen-test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cmath>
 #include <random>
 #include <string>
@@ -9,6 +10,9 @@ using namespace std;
 random_device r;
 mt19937 rgen(r());
 uniform_real_distribution<> rdis(0,1);
+
+//std::ostream* ostr[2] { &std::cout, &ofstr };
+std::vector<std::ostream*> output;
 
 const  int N_max = 100;
 int        N_test {};
@@ -119,21 +123,29 @@ int target(int from)
 void dh(int from)
 {
   int to = target(from);
-  cout.precision(3);
-  cout << "<dh from='" << from << "' to='" << to << "'"
-       << " val='" << Point[to].z - Point[from].z << "'"
-       << " stdev='2.0' />\n";
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;
+      cout.precision(3);
+      cout << "<dh from='" << from << "' to='" << to << "'"
+           << " val='" << Point[to].z - Point[from].z << "'"
+           << " stdev='2.0' />\n";
+    }
 }
 
 void dist(int from)
 {
   int to = target(from);
-  cout.precision(2);
-  double dx = Point[to].x - Point[from].x;
-  double dy = Point[to].y - Point[from].y;
-  double d  = sqrt(dx*dx + dy*dy);
-  cout << "<distance from='" << from << "' to='" << to << "'"
-       << " val='" << d << "' stdev='4.0' />\n";
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;
+      cout.precision(4);
+      double dx = Point[to].x - Point[from].x;
+      double dy = Point[to].y - Point[from].y;
+      double d  = sqrt(dx*dx + dy*dy);
+      cout << "<distance from='" << from << "' to='" << to << "'"
+           << " val='" << d << "' stdev='4.0' />\n";
+    }
 }
 
 void sdist(int from)
@@ -141,18 +153,30 @@ void sdist(int from)
   Dh dh;
 
   int to = target(from);
-  cout.precision(2);
   double dx = Point[to].x - Point[from].x;
   double dy = Point[to].y - Point[from].y;
   double dz = Point[to].z - Point[from].z + dh.diff();
   double d  = sqrt(dx*dx + dy*dy + dz*dz);
-  cout << "<s-distance from='" << from << "' to='" << to << "'"
-       << " val='" << d << "'" << dh << " stdev='5.0' />\n";
+  bool first=true;
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;
+      cout.precision(4);
+      if (first) {
+          cout << "<s-distance from='" << from << "' to='" << to << "'"
+               << " val='" << d << "'" << dh << " stdev='5.0' />\n";
+          first=false;
+        }
+      else {
+          cout << "<s-distance from='" << from << "' to='" << to << "'"
+               << " val='" << d << "'" << dh << " stdev='5.0' />\n";
+        }
+    }
 }
 
 void dir(int from)
 {
-  cout.precision(4);
+
   for (int i=0; i<=1+random_index(4); i++)
     {
       int to = target(from);
@@ -160,8 +184,13 @@ void dir(int from)
       double dy = Point[to].y - Point[from].y;
       double s  = atan2(dy, dx)/M_PI*200;
       if (s < 0) s += 400;
-      cout << "<direction to='" << to << "'"
-           << " val='" << s << "' stdev='6.0' />\n";
+      for (auto ostr : output)
+        {
+          auto& cout = *ostr;
+          cout.precision(4);
+          cout << "<direction to='" << to << "'"
+               << " val='" << s << "' stdev='6.0' />\n";
+        }
     }
 }
 
@@ -182,24 +211,57 @@ void zen(int from)
     }
   while(s == 0);
   double z  = acos(dz/s)/M_PI*200;
-  cout << "<z-angle from='" << from << "' to='" << to << "'"
-       << " val='" << z << "'" << dh << " stdev='7.0' />\n";
+  bool first=true;
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;
+      cout.precision(4);
+
+      if (first) {
+          cout << "<z-angle from='" << from << "' to='" << to << "'"
+               << " val='" << z << "'" << dh << " stdev='7.0' />\n";
+          first=false;
+        }
+      else {
+          cout << "<z-angle from='" << from << "' to='" << to << "'"
+               << " val='" << z << "'" << dh << " stdev='7.0' />\n";
+        }
+    }
 }
 
 vector<Function> obs = {  dir, dist, sdist, zen };
 
 void observations(int from)
 {
-  cout << "<obs from='" << from << "'>\n";
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;
+      cout << "<obs from='" << from << "'>\n";
+    }
+
   for (int i=0; i<=random_index(5); i++) obs[random_index(obs.size())](from);
-  cout << "</obs>\n\n";
+
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;
+      cout << "</obs>\n\n";
+    }
 }
 
 void heights(int from)
 {
-  cout << "<height-differences>\n";
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;
+      cout << "<height-differences>\n";
+    }
+
   for (int i=0; i<=random_index(5); i++) dh(from);
-  cout << "</height-differences>\n\n";
+
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;cout << "</height-differences>\n\n";
+    }
 }
 
 void coordinates(int from)
@@ -219,43 +281,56 @@ void coordinates(int from)
 
   int dim=0;
 
-  cout << "<coordinates>\n";
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;
+      cout << "<coordinates>\n";
+    }
 
   for (int i=0; i<=random_index(3); i++)
     {
-      cout << "<point id='" << p[i] << "'";
       int k = random_index(3);
-      switch (k)
+      for (auto ostr : output)
         {
-        case 0:
-          dim += 3;
-          cout << " x='" << Point[p[i]].x << "'";
-          cout << " y='" << Point[p[i]].y << "'";
-          cout << " z='" << Point[p[i]].z << "'";
-          break;
-        case 1:
-          dim += 2;
-          cout << " x='" << Point[p[i]].x << "'";
-          cout << " y='" << Point[p[i]].y << "'";
-          break;
-        case 2:
-          dim += 1;
-          cout << " z='" << Point[p[i]].z << "'";
-          break;
-        };
-      cout << " />\n";
+          auto& cout = *ostr;
+          cout.precision(3);
+          cout << "<point id='" << p[i] << "'";
+          switch (k)
+            {
+            case 0:
+              dim += 3;
+              cout << " x='" << Point[p[i]].x << "'";
+              cout << " y='" << Point[p[i]].y << "'";
+              cout << " z='" << Point[p[i]].z << "'";
+              break;
+            case 1:
+              dim += 2;
+              cout << " x='" << Point[p[i]].x << "'";
+              cout << " y='" << Point[p[i]].y << "'";
+              break;
+            case 2:
+              dim += 1;
+              cout << " z='" << Point[p[i]].z << "'";
+              break;
+            };
+          cout << " />\n";
+        }
     }
 
-  cout << "<cov-mat dim='" << dim << "' band='" << dim-1 << "'>\n";
-  for (int s=1, i=1; i<=dim; i++)
+  for (auto ostr : output)
     {
-      cout << "25";
-      for (int k=1; k<=dim-1 && i+k<=dim; k++, s=-s) cout << " " << s;
-      cout << endl;
-    }
-  cout << "</cov-mat>\n";
+      auto& cout = *ostr;
+      cout << "<cov-mat dim='" << dim << "' band='" << dim-1 << "'>\n";
+      for (int s=1, i=1; i<=dim; i++)
+        {
+          cout << "25";
+          for (int k=1; k<=dim-1 && i+k<=dim; k++, s=-s) cout << " " << s;
+          cout << endl;
+        }
+      cout << "</cov-mat>\n";
 
-  cout << "</coordinates>\n\n";
+      cout << "</coordinates>\n\n";
+    }
 }
 
 void vectors(int from)
@@ -275,36 +350,57 @@ void vectors(int from)
 
   int dim=0;
 
-  cout << "<vectors>\n";
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;
+      cout << "<vectors>\n";
+    }
 
   for (int i=0; i<=random_index(3); i++)
     {
       dim += 3;
       int to = target(p[i]);
-      cout << "<vec from='" << p[i] << "' to='" << to << "'"
-           << " dx='" << Point[to].x - Point[p[i]].x << "'"
-           << " dy='" << Point[to].y - Point[p[i]].y << "'"
-           << " dz='" << Point[to].z - Point[p[i]].z << "'"
-           << " />\n";
+      for (auto ostr : output)
+        {
+          auto& cout = *ostr;
+          cout.precision(3);
+          cout << "<vec from='" << p[i] << "' to='" << to << "'"
+               << " dx='" << Point[to].x - Point[p[i]].x << "'"
+               << " dy='" << Point[to].y - Point[p[i]].y << "'"
+               << " dz='" << Point[to].z - Point[p[i]].z << "'"
+               << " />\n";
+        }
     }
 
-  cout << "<cov-mat dim='" << dim << "' band='" << dim-1 << "'>\n";
-  for (int s=1, i=1; i<=dim; i++)
+  for (auto ostr : output)
     {
-      cout << "25";
-      for (int k=1; k<=dim-1 && i+k<=dim; k++, s=-s) cout << " " << s;
-      cout << endl;
-    }
-  cout << "</cov-mat>\n";
+      auto& cout = *ostr;
+      cout << "<cov-mat dim='" << dim << "' band='" << dim-1 << "'>\n";
+      for (int s=1, i=1; i<=dim; i++)
+        {
+          cout << "25";
+          for (int k=1; k<=dim-1 && i+k<=dim; k++, s=-s) cout << " " << s;
+          cout << endl;
+        }
+      cout << "</cov-mat>\n";
 
-  cout << "</vectors>\n\n";
+      cout << "</vectors>\n\n";
+    }
 }
 
 // #######################################################################
 
 int main(int argc, char* argv[])
 {
-  cout.setf(ios::fixed, ios::floatfield);
+  std::ofstream ofstr("../gen-test/pokus-2.gkf");
+  output.push_back(&std::cout);
+  output.push_back(&ofstr);
+
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;
+      cout.setf(ios::fixed, ios::floatfield);
+    }
 
   do
     N_test = random_index(N_max);
@@ -317,26 +413,41 @@ int main(int argc, char* argv[])
       Point[i].z = xyz();
     }
 
-  cout << "<?xml version='1.0' ?>\n"
-       << "<gama-local xmlns='http://www.gnu.org/software/gama/gama-local'>\n"
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;
+      cout << "<?xml version='1.0' ?>\n"
+           << "<gama-local xmlns='http://www.gnu.org/software/gama/gama-local'>\n"
 
-       << "<network>\n\n"
+           << "<network>\n\n"
 
-       << "<description>\n"
-       << "gen-test 2.00\n"
-       << "</description>\n\n"
+           << "<description>\n"
+           << "gen-test 3.00\n"
+           << "</description>\n\n"
 
-       << "<points-observations>\n\n";
+           << "<points-observations>\n\n";
+    }
 
-  cout.precision(2);
   for (int i=0; i<N_test; i++)
-    cout << "<point id='" << i << "'"
-         << " x='" << Point[i].x << "'"
-         << " y='" << Point[i].y << "'"
-         << " z='" << Point[i].z << "'"
-         << point_type[random_point_type_index()]
-         << "/>\n";
-  cout << endl;
+    {
+      auto pt = point_type[random_point_type_index()];
+      for (auto ostr : output)
+        {
+          auto& cout = *ostr;
+          cout.precision(2);
+          cout << "<point id='" << i << "'"
+               << " x='" << Point[i].x << "'"
+               << " y='" << Point[i].y << "'"
+               << " z='" << Point[i].z << "'"
+               << pt
+               << "/>\n";
+        }
+    }
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;
+      cout << endl;
+    }
 
   for (int i=0; i<N_test; i++)
     for (int j=0; j<random_index(3); j++)
@@ -344,8 +455,12 @@ int main(int argc, char* argv[])
         cluster[random_index(cluster.size())](i);
       }
 
-  cout << "</points-observations>\n"
-       << "</network>\n"
-       << "</gama-local>\n\n";
+  for (auto ostr : output)
+    {
+      auto& cout = *ostr;
+      cout << "</points-observations>\n"
+           << "</network>\n"
+           << "</gama-local>\n\n";
+    }
 }
 
